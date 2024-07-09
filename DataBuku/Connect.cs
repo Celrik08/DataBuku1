b5bf04ea53d7f14dc2ac20fb9be6afdd73350b0c
Celrik08 <m.fajar080907@gmail.com>
2024-07-09 12:10:00 +0800

    Add project files.

---
+++ DataBuku/Connect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataBuku
+{
+    internal class Connect
+    {
+        public static SqlConnection GetConn()
+        {
+            SqlConnection Conn = new SqlConnection();
+            Conn.ConnectionString = "Data Source=MSI\\SQLEXPRESS;Initial Catalog=perpustakaan;Integrated Security=True";
+            return Conn;
+        }
+    }
+}
